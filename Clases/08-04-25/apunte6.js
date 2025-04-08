69000736cc87f67a4fc28da45511241e648ebde2
Pedro <400091@sistemas.frc.utn.edu.ar>
2025-04-08 18:23:39 -0200

    Primeros avances con JS

---
+++ Clases/08-04-25/apunte6.js
@@ -0,0 +1,23 @@
+/* VARIABLES */
+let x = 10;
+console.log(x);
+console.log("HOla soy pedro");
+
+nombre = "Pedro";
+apellido = "Torcivia";
+let mensaje = nombre + " " + apellido;
+console.log(mensaje);
+console.log(mensaje[0]);
+console.log(mensaje.length);
+console.log('Hola mi nombre es ${mensaje}');
+
+console.log(nombre);
+const PI = 3.14; 
+console.log("La largatija: \n Cosmica")
+
+monto = 100;
+if (monto > 60) {
+    console.log("monto mayor");
+} else {
+    console.log("menor")
+}
