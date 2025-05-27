28be8afed9f366907ea4b13e8920a03b20dc2f07
Pedro Torcivia <torcivia.p3dro@gmail.com>
2025-05-27 09:11:10 -0200

    Etapa 1 - Frontend 2025

--- PasoAPaso/dds-frontend/src/App.jsx
+++ PasoAPaso/dds-frontend/src/App.jsx
@@ -1,40 +1,9 @@
-import "./App.css";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import { Inicio } from "./components/Inicio";
-import { ArticulosFamilias } from "./components/ArticulosFamilias";
-import { Menu } from "./components/Menu";
-import { Footer } from "./components/Footer";
-import { Articulos } from "./components/articulos/Articulos";
-import { ModalDialog } from "./components/ModalDialog";
-import { ArticulosJWT } from "./components/articulosJWT/ArticulosJWT";
-import { RequireAuth } from "./components/RequiereAuth";
-import { Login } from "./components/login/Login";
-
+import {Inicio} from "./components/Inicio";
 function App() {
-  return (
-    <BrowserRouter>
-      <ModalDialog />
-      <Menu />
-      <div className="divBody  px-4">
-        <Routes>
-          <Route path="/inicio" element={<Inicio />} />
-          <Route path="/articulosfamilias" element={<ArticulosFamilias />} />
-          <Route path="/articulos" element={<Articulos />} />
-          <Route
-            path="/articulosjwt"
-            element={
-              <RequireAuth rol="admin">
-                <ArticulosJWT />
-              </RequireAuth>
-            }
-          />
-          <Route path="/login/:componentFrom" element={<Login />} />
-          <Route path="*" element={<Navigate to="/inicio" replace />} />
-        </Routes>
-      </div>
-      <Footer />
-    </BrowserRouter>
-  );
+return (
+    <>
+        <Inicio/>
+    </>
+);
 }
-
 export default App;
--- PasoAPaso/dds-frontend/src/components/Inicio.jsx
+++ PasoAPaso/dds-frontend/src/components/Inicio.jsx
@@ -1,21 +1,18 @@
-import { Link } from "react-router-dom";
-
 function Inicio() {
   return (
     <div className="mt-4 p-5 rounded" style={{ backgroundColor: "lightgray" }}>
-      <h1>Pymes 2024</h1>
+      <h1>Pymes 2025</h1>
       <p>Este ejemplo está desarrollado con las siguientes tecnologías:</p>
       <p>
-        Backend: NodeJs, Express , WebApiRest, Swagger, Sequelize, Sqlite
-        múltiples capas en Javascript.
+        Backend: NodeJs, Express , WebApiRest, Swagger, Sequelize, Sqlite y Javascript.
       </p>
       <p>
         Frontend: Single Page Application, HTML, CSS, Bootstrap, Javascript, NodeJs y React.
       </p>
-      <Link to="/articulosfamilias" className="btn btn-lg btn-primary">
-            <i className="fa fa-search"> </i>  Ver Articulos Familias
-       </Link>
-
+      <button className="btn btn-lg btn-primary">
+        <i className="fa fa-search"> </i>
+        Ver Categorias
+      </button>
     </div>
   );
 }
