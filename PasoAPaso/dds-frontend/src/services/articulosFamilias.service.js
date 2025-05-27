56c8de1707ead42603701b80521379be5fa49f40
Pedro Torcivia <torcivia.p3dro@gmail.com>
2025-05-27 09:22:03 -0200

    Etapa 1 - Frontend 2025

--- PasoAPaso/dds-frontend/src/services/articulosFamilias.service.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const urlResource = "https://labsys.frc.utn.edu.ar/dds-backend-2024/api/articulosfamilias";
-async function Buscar() {
-  const resp = await axios.get(urlResource);
-  return resp.data;
-}
-export const articulosFamiliasService = {
-  Buscar
-};
